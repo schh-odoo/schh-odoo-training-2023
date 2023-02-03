0e1dc29c919fd13e49c4097e93dfce12702cae93
Pratik Raval <prra@odoo.com>
2023-02-03 16:24:09 +0530

    Removed the Open Academy Module

--- open_academy/__manifest__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-{
-	'name' : 'Open Academy',
-	'description' : 'Open Academy Module',
-	'application' : True,
-}
--- open_academy/models/course.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-class openAcademyCourse(models.Model):
-	_name = 'academy.course'
-	_description = 'Open Academy Model for Course'
-
-	name = fields.Char() # Integer()
