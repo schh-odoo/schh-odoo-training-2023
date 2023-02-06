400837307493673b2e429c45b1e95d5386df2684
Aman <66776559+amanpatel10@users.noreply.github.com>
2023-02-06 20:44:03 +0530

    Revert "Merge pull request #27 from prra-odoo/master-real-estate-ampa"
    This reverts commit ace4d6b55b2ce9349dc0518568e93555ca342399, reversing
    changes made to 0e1dc29c919fd13e49c4097e93dfce12702cae93.

--- estate/__manifest__.py
+++
@@ -1,9 +0,0 @@
-#--coding: utf-8 -
-{
-    'name': "Aman Real Estate",
-    'depends': [],
-    'application': True,
-    'data':['security/ir.model.access.csv',
-            'views/estate_property_views.xml',
-            'views/estate_menus.xml']
-}
